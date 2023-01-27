52690faf168310503d8fddf8223bd8d0f842b2e2
AAswordman <1002153674@qq.com>
2023-01-27 18:43:44 +0800

    over

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,1 +1,6 @@
-console.log("123".split(''))
+
+const task_arr = ["Ao", "Jf", "Sk", "Ch", "Om", "Bs", "Hd", "Oa", "Gx", "Xe"];
+function taskTranToNum(t: string) {
+    return t.split(" ").map(e => task_arr.indexOf(e)).join('');
+}
+console.log(taskTranToNum("003"));
