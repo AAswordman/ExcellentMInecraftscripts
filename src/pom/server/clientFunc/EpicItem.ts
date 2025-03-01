f7a026b12f1a9773832e5fa49827e2637f8eaa65
EveyLoews <137289596+EveyLoews@users.noreply.github.com>
2025-03-01 14:00:47 +0800

    回响2技能

--- src/pom/server/clientFunc/EpicItem.ts
+++ src/pom/server/clientFunc/EpicItem.ts
@@ -19,6 +19,8 @@ import MathUtil from '../../../modules/exmc/utils/math/MathUtil.js';
 import { TickEvent } from '../../../modules/exmc/server/events/events.js';
 import ItemTagComponent, { ItemTagComponentType } from '../data/ItemTagComponent.js';
 import { get } from 'http';
+import { IVector3 } from '../../../modules/exmc/utils/math/Vector3';
+import Vector2 from '../../../modules/exmc/utils/math/Vector2.js';
 
 export default class EpicItemUse extends GameController {
 
@@ -63,7 +65,9 @@ export default class EpicItemUse extends GameController {
         return q.getEntities();
      }
   rangeCircle(user : Entity , point : Vector3, r : number, h : number) {
-      let q = new ExEntityQuery(user.dimension).at(point)
+      let q = new ExEntityQuery(user.dimension)
+          .at(point)
+          .facingByDirection(point, 0) 
           .queryCircle(r, h,{
               excludeTags:["wbmsyh"],
               excludeTypes: ["item"]
@@ -136,16 +140,17 @@ export default class EpicItemUse extends GameController {
 
       if (this.isHoldingItem("epic:echoing_scream_saber")) {
         let cd = this.getCd(item);
-        if (cd == 0) {
-          const tmpV = new Vector3();
-          const base_atk = this.getAttack(item);
-          const sharpness = this.getEnchantLevel("sharpness");
-          const atk = base_atk + sharpness * 1.25;
-          //let eff_atk = base_atk*(1.25^strength)/(1.25^weakness)
-          //切割倍率
+        const tmpV = new Vector3();
+        const base_atk = this.getAttack(item);
+        const sharpness = this.getEnchantLevel("sharpness");
+        const atk = base_atk + sharpness * 1.25;
+        //let eff_atk = base_atk*(1.25^strength)/(1.25^weakness)
+        //重击
+        if (cd == 0 && this.player.isSneaking == false) {
+          //切割倍率200%+5
           const dam1 = 2.0 * atk + 5;
-          //回声爆破伤害（法术）
-          const dam2 = 30;
+          //回声爆破伤害35（法术）
+          const dam2 = 35;
           const loc = new Vector3(this.player.location);
           let shock_entity : Entity[] = [];
           //第一段切割
@@ -165,7 +170,7 @@ export default class EpicItemUse extends GameController {
                   let direction = tmpV.set(entity.location).sub(this.player.location).normalize();
                   entity.applyKnockback(direction.x, direction.z, 1, 0.5);
                   if (echoRecord >= 3) {
-                    entity.setDynamicProperty('echo_record', 0);
+                    entity.setDynamicProperty('echo_record', echoRecord-3);
                     shock_entity.push(entity);
                     entity.addEffect("slowness",2 * 20,
                       {
@@ -184,13 +189,16 @@ export default class EpicItemUse extends GameController {
              this.spawnPar("minecraft:sonic_explosion",targetloc,new Vector3(0.1,0.6,0.1));
              this.spawnPar("minecraft:sonic_explosion",targetloc,new Vector3(0,0.4,0));
              this.spawnPar("minecraft:sonic_explosion",targetloc,new Vector3(-0.1,0.2,-0.1));
-             this.runTimeout(() => {
+             //this.getDimension().playSound("mob.warden.sonic_charge",targetloc,{"volume":50});
+
+            this.runTimeout(() => {
             for (let entity of this.rangeBall(this.player,targetloc,3)) {
               try {
+                let targetloc = new Vector3(target.location);
                   let echoRecord = Number(entity.getDynamicProperty('echo_record')) || 0;
                   if (entity === target) {
                     this.spawnPar("minecraft:critical_hit_emitter",targetloc,new Vector3(0,1.8,0))
-                    this.getDimension().playSound("mob.warden.sonic_boom",target.location,{"volume":50});
+                    this.getDimension().playSound("mob.warden.sonic_boom",targetloc,{"volume":50});
                   }
                   let Dam = (entity === target) ? dam2 : (0.5 * dam2);
                   entity.applyDamage(Dam, {
@@ -202,12 +210,94 @@ export default class EpicItemUse extends GameController {
               }
               catch (e) { }
             }
-          },400)
-        }
+            },400);
+          }
         },800);
-        }      
       }
-    });
+        if (cd == 0 && this.player.isSneaking) {
+          const dam1 = 2.5 * atk + 10;
+          let dam2 = 10;
+          const loc = new Vector3(this.player.location);
+          const face = new Vector3(this.player.getViewDirection());
+          let locFin = new Vector3(loc.x+face.x*2,loc.y+face.y*2,loc.z+face.z*2)
+          let shock_entity : Entity[] = [];
+          //第一段切割
+          this.runTimeout(() => {
+            //this.exPlayer.getScoresManager().removeScore("wbfl", 25);
+            this.player.startItemCooldown("saber", 5 * 20);
+            for (let entity of this.rangeBall(this.player,locFin,5)) {
+              try {
+                  let Dam = dam1
+                  let echoRecord = Number(entity.getDynamicProperty('echo_record')) || 0;
+                  //回声印记增伤
+                  Dam *= (1 + 0.1 * echoRecord)
+                  entity.applyDamage(Dam, {
+                    "cause": EntityDamageCause.contact,
+                    "damagingEntity": this.player
+                  });
+                  let direction = tmpV.set(entity.location).sub(this.player.location).normalize();
+                  entity.applyKnockback(direction.x, direction.z, 1, 0.5);
+                  if (echoRecord >= 5) {
+                    shock_entity.push(entity);
+                    entity.addEffect("slowness",4 * 20,
+                      {
+                        "amplifier": 5,
+                        "showParticles": false
+                      })
+                  }
+                }
+              catch (e) { }
+            }
+            }, 0);
+            this.runTimeout(() => {
+              for (let target of shock_entity) {
+              let tloc = new Vector3(target.location);
+              const targetloc = tloc as Vector3;
+              let echoRecord = Number(target.getDynamicProperty('echo_record')) || 0;
+              const TimeLine = ExSystem.timeLine(this, {
+                "0.1": (time) => { 
+                  this.getDimension().playSound("mob.warden.sonic_charge",targetloc,{"volume":1,"pitch":2.5});
+                 },
+
+                "0.5": (time) => { 
+                  this.spawnPar("epic:echoing_scream_saber_particle3",targetloc,new Vector3(0,0,0));
+                 },
+                "0.65": (time) => { 
+                  this.getDimension().playSound("mob.warden.dig",targetloc,{"volume":0.8,"pitch":2.0});
+                 },
+                "1.0": (time) => {
+                  this.spawnPar("minecraft:critical_hit_emitter",targetloc,new Vector3(0,1.8,0));
+                  this.getDimension().playSound("item.trident.thunder",targetloc,{"volume":2.0,"pitch":1.5});
+                  this.getDimension().playSound("item.trident.thunder",loc,{"volume":1.0,"pitch":3});
+                 },
+                "1.1": (time) => {
+                  this.player.runCommand("/camerashake add @s 0.4 0.08 rotational");
+                  this.player.runCommand("/camerashake add @s 1.5 0.15 positional");
+                },
+                "1.2": (time) => {
+                  try {
+                    let Dam = ((echoRecord>5) ? dam2 + 2*(echoRecord-5) : dam2)*echoRecord;
+                    target.applyDamage(Dam, {
+                      "cause": EntityDamageCause.magic,
+                      "damagingEntity": this.player
+                    });
+                    target.setDynamicProperty('echo_record', 0);
+                    let direction = tmpV.set(target.location).sub(this.player.location).normalize();
+                    target.applyKnockback(direction.x, direction.z, 1, 0.5);
+                 }
+                 catch (e) { }
+                 },
+                 "1.3": (time) => {
+                  this.getDimension().playSound("item.trident.thunder",targetloc,{"volume":1.0,"pitch":2.0});
+                 },
+              });
+              TimeLine.start();
+            }
+        },400);
+      }
+    }
+  //End 
+  });
 
   }
   onLoad(): void {
